80d9ee6311d35d1c551d583b6445cacfd4d5aba2
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-11-14 21:34:47 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+    const oneSecond = 1000;
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, oneSecond);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID)
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('app'));
